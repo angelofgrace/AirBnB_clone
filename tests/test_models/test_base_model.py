d75b6236bb62ac919767b3f752fee9f3723d5182
chrisvanndy <chrisvannyc@gmail.com>
2021-05-18 20:17:11 +0000

    unit test suite for task 4

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""unit test"""
+
+
+from models.base_model import BaseModel
+import unittest
+import datetime
+
+class Test_base_model(unittest.TestCase):
+    """Test_base_model class for unit tests"""
+
+    def test_id(self):
+        model1 = BaseModel()
+        self.assertNotEqual(model1.id, "")
+
+    def test_created_at(self):
+        model1 = BaseModel()
+        self.assertNotEqual(model1.created_at, "")
+
+    def test_str(self):
+        model2 = BaseModel()
+        self.assertIn(model2.id, model2.__str__())
+        self.assertIn(str(model2.__dict__), model2.__str__())
+        self.assertIn(model2.__class__.__name__, model2.__str__())
+
+    def test_to_dict(self):
+        model3 = BaseModel()
+        tester3 = model3.to_dict()
+        self.assertIsInstance(tester3, dict)
+
+    def test_save(self):
+        model4 = BaseModel()
+        self.assertNotEqual(model4, model4.save())
+       
