d72c0de38a57e831321f85b8a494e7666acb2141
chrisvanndy <chrisvannyc@gmail.com>
2021-05-22 23:37:28 +0000

    updated child class modules

---
+++ models/place.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Module creates State class"""
+
+from models.base_model import BaseModel
+from datetime import datetime
+
+
+class Place(BaseModel):
+
+    """State is a child class of BaseModel with relevante class attributes"""
+
+    city_id = ""  # will be City.id
+    user_id = ""  # will be User.id
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = [""]  # will be the amenity.id
+
+    def __init__(self, *args, **kwargs):
+        if not kwargs:
+            super().__init__()
+        else:
+            for k in kwargs.keys():
+                # iterating through keys/value pairs k = keys
+                if k == 'created_at' or k == 'updated_at':
+                    kwargs[k] = datetime.strptime(
+                        kwargs[k],
+                        '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, k, kwargs[k])
+                # everything except for class attr are set
+                elif k != '__class__':
+                    setattr(self, k, kwargs[k])
