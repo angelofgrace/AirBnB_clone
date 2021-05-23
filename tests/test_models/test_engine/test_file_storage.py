dd412b8da91da5fa3980c8ce86fd3dee0721176a
Grace Fallon <graceleefallon@gmail.com>
2021-05-23 19:38:08 +0000

    Tests added for reload and save

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -17,7 +17,15 @@ class Test_file_storage(unittest.TestCase):
         self.assertNotEqual("file.json", "")
 
     def test__objects(self):
+        """ works for all() and new() """
         model5 = BaseModel()
         storage2 = FileStorage()
         self.assertIn("{}.{}".format(model5.__class__.__name__, model5.id), storage2.all().keys())
-        self.assertIn(model5, storage2.all().values())   
+        self.assertIn(model5, storage2.all().values())
+
+    def test_save(self):
+        model6 = BaseModel()
+        storage3 = FileStorage()
+        FileStorage.save(model6)
+        storage3.reload()
+        self.assertIn("{}.{}".format(model6.__class__.__name__, model6.id), storage3.all().keys())
